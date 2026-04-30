9d841720c885701e9336def86c503896c63d40cd
gustavo1904slb <a14480@oficina.pt>
2026-04-30 16:54:55 +0100

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using _14480_employes_managment.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _14480_employes_managment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/UserActivity.cs
@@ -0,0 +1,10 @@
+﻿namespace _14480_employes_managment.Models
+{
+    public class UserActivity
+    {
+        public string? CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string? ModifiedById { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
